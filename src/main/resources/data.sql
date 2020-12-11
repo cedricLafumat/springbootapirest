5dede31ecc767efaf709676a9679c397e73b9773
cedricLafumat <cedric.lafumat@le-campus-numerique.fr>
2020-12-11 09:41:57 +0100

    Modification du scenario de test pour avoir une seul fonction de test dans l'ordre

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO car VALUES(1, 'Fiesta' , 'Ford', 'noire');
-INSERT INTO car VALUES(2, 'Mondeo' , 'Ford', 'gris');
-INSERT INTO car VALUES(3, 'A7' , 'Audi', 'noire');
+INSERT INTO car(id, model, marque, color) VALUES(1, 'Fiesta' , 'Ford', 'noire');
+INSERT INTO car(id, model, marque, color) VALUES(2, 'Mondeo' , 'Ford', 'gris');
+INSERT INTO car(id, model, marque, color) VALUES(3, 'A7' , 'Audi', 'noire');
--- src/test/java/com/campus/SpringBootApiRest/SpringBootApiRestApplicationTests.java
+++ src/test/java/com/campus/SpringBootApiRest/SpringBootApiRestApplicationTests.java
@@ -23,6 +23,47 @@ class SpringBootApiRestApplicationTests {
 	private TestRestTemplate testRestTemplate;
 
 	@Test
+	public void testCar(){
+		//récupère tout les vehicules
+		String resultRequest1 = this.testRestTemplate.getForObject("/car", String.class);
+		assertEquals("[{\"id\":1,\"model\":\"Fiesta\",\"marque\":\"Ford\",\"color\":\"noire\"}," +
+				"{\"id\":2,\"model\":\"Mondeo\",\"marque\":\"Ford\",\"color\":\"gris\"}," +
+				"{\"id\":3,\"model\":\"A7\",\"marque\":\"Audi\",\"color\":\"noire\"}]", resultRequest1);
+
+		//Ajoute un vehicule
+		Car addingCar = new Car (4, "Focus", "Ford", "gris");
+		String resultRequest2 = this.testRestTemplate.postForObject("/car", addingCar, String.class);
+		assertEquals("{\"id\":4,\"model\":\"Focus\",\"marque\":\"Ford\",\"color\":\"gris\"}", resultRequest2);
+
+		//Vérification de l'ajout du véhicule
+		String resultRequest3 = this.testRestTemplate.getForObject("/car", String.class);
+		assertEquals("[{\"id\":1,\"model\":\"Fiesta\",\"marque\":\"Ford\",\"color\":\"noire\"}," +
+				"{\"id\":2,\"model\":\"Mondeo\",\"marque\":\"Ford\",\"color\":\"gris\"}," +
+				"{\"id\":3,\"model\":\"A7\",\"marque\":\"Audi\",\"color\":\"noire\"}," +
+				"{\"id\":4,\"model\":\"Focus\",\"marque\":\"Ford\",\"color\":\"gris\"}]", resultRequest3);
+
+		//Modification d'un véhicule et vérification de la modification
+		Car modifyCar = new Car (2, "Mondeo", "Ford", "orange");
+		this.testRestTemplate.put("/car/2", modifyCar, String.class);
+		String resultRequest4 = this.testRestTemplate.getForObject("/car/2", String.class);
+		assertEquals("{\"id\":2,\"model\":\"Mondeo\",\"marque\":\"Ford\",\"color\":\"orange\"}", resultRequest4);
+
+		//Vérification de la liste des véhicules après modif
+		String resultRequest5 = this.testRestTemplate.getForObject("/car", String.class);
+		assertEquals("[{\"id\":1,\"model\":\"Fiesta\",\"marque\":\"Ford\",\"color\":\"noire\"}," +
+				"{\"id\":2,\"model\":\"Mondeo\",\"marque\":\"Ford\",\"color\":\"orange\"}," +
+				"{\"id\":3,\"model\":\"A7\",\"marque\":\"Audi\",\"color\":\"noire\"}," +
+				"{\"id\":4,\"model\":\"Focus\",\"marque\":\"Ford\",\"color\":\"gris\"}]", resultRequest5);
+
+		//Suppression d'un véhicule
+		testRestTemplate.delete("/car/3", 3);
+		String resultRequest6 = this.testRestTemplate.getForObject("/car", String.class);
+		assertEquals("[{\"id\":1,\"model\":\"Fiesta\",\"marque\":\"Ford\",\"color\":\"noire\"}," +
+				"{\"id\":2,\"model\":\"Mondeo\",\"marque\":\"Ford\",\"color\":\"orange\"}," +
+				"{\"id\":4,\"model\":\"Focus\",\"marque\":\"Ford\",\"color\":\"gris\"}]", resultRequest6);
+	}
+
+	/*@Test
 	public void getAllCars() {
 		String resultRequest = this.testRestTemplate.getForObject("/car", String.class);
 		assertEquals("[{\"id\":1,\"model\":\"Fiesta\",\"marque\":\"Ford\",\"color\":\"noire\"}," +
@@ -62,6 +103,6 @@ class SpringBootApiRestApplicationTests {
 		String resultRequest = this.testRestTemplate.getForObject("/car", String.class);
 		assertEquals("[{\"id\":1,\"model\":\"Fiesta\",\"marque\":\"Ford\",\"color\":\"noire\"}," +
 				"{\"id\":2,\"model\":\"Mondeo\",\"marque\":\"Ford\",\"color\":\"gris\"}]", resultRequest);
-	}
+	}*/
 
 }
